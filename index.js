055033eeba50aaae33718666cfba83dc154584d0
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-01-26 17:32:52 +0100

    FIX

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const recuperaLibri = async function () {
     if (libri.ok) {
       let data = await libri.json();
       let contenitore = document.querySelector("#container-cards");
+      let cardNumber = 1;
       data.forEach((book) => {
+        cardNumber++;
         contenitore.innerHTML =
           contenitore.innerHTML +
-          `  <div class="card col-5 col-md-3 m-3 p-0">
+          `  <div id="${cardNumber}" class="card col-5 col-md-3 m-3 p-0">
                 <img src="${book.img}" class="img-fluid" style="height: 60%" alt="Foto Libro">
                 <div class="card-body p-3 d-flex flex-column justify-content-between ">
                   <h5 class="card-title">${book.title}</h5>
                   <h3 class="card-text"> ${book.price} €</h3>
-                  <a class="btn btn-primary" onclick="buttonSkip()">NASCONDI</a>
+                  <a class="btn btn-primary" onclick="removeCard(event)">NASCONDI</a>
                 </div>
               </div> `;
       });
@@ -24,16 +26,8 @@ const recuperaLibri = async function () {
   }
 };
 
+function removeCard(e) {
+  e.target.parentElement.parentElement.style.display = "none";
+}
+
 recuperaLibri();
-
-/*function buttonSkip() {
-    const nascondiLibri = async function () {
-        try {
-          let libri = await fetch("https://striveschool-api.herokuapp.com/books");
-          if (libri.ok) {
-            let data = await libri.json();
-            data.forEach ((book) =>{
-                data.splice(book, 1)
-            })
-
-}*/
